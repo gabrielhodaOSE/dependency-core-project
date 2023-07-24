b6f35ab4d282feb1622853a6927ec4660792e49d
gabriel.hoda <gabriel.hoda@orange.com>
2023-07-24 15:18:05 +0300

    branch 2

--- src/main/java/org/example/CoreProjectUtils.java
+++ src/main/java/org/example/CoreProjectUtils.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class CoreProjectUtils {
     public static void coreProjectFunction() {
-        System.out.println("This is NOT a function in the core project");
+        System.out.println("This is a function in the core project");
+        System.out.println("branch2");
     }
 }
