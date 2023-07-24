6df9f4964c5fd83d8eb3ba3e5882f8e0e1b02364
gabriel.hoda <gabriel.hoda@orange.com>
2023-07-24 12:24:17 +0300

    initial commit

---
+++ src/main/java/org/example/CoreProjectUtils.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class CoreProjectUtils {
+    public static void coreProjectFunction() {
+        System.out.println("This is NOT a function in the core project");
+    }
+}
